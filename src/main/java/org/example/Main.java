b33002d5b8600d4540db5a07bd18315cab35e662
frimanik <lotos-222@protonmail.com>
2024-01-31 18:21:11 +0800

    Implementation of primitive StringBuilder class with an undo() method based on Memento pattern.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.StringBuilderImplementation.CustomStringBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CustomStringBuilder customStringBuilder = new CustomStringBuilder();
+        customStringBuilder.append("one");
+        customStringBuilder.append("two");
+        customStringBuilder.insert(3, "three");
+        System.out.println(customStringBuilder);
+        customStringBuilder.delete(3, 8);
+        System.out.println(customStringBuilder);
+        customStringBuilder.undo();
+        System.out.println(customStringBuilder);
     }
 }
---
+++ src/main/java/org/example/StringBuilderImplementation/CustomStringBuilder.java
@@ -0,0 +1,121 @@
+package org.example.StringBuilderImplementation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomStringBuilder{
+
+    private char[] value;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 16;
+    private List<CustomStringBuilderMemento> history;
+
+    public CustomStringBuilder() {
+        value = new char[DEFAULT_CAPACITY];
+        size = 0;
+        history = new ArrayList<>();
+    }
+
+    public CustomStringBuilder(int capacity) {
+        value = new char[capacity];
+        size = 0;
+    }
+
+    public CustomStringBuilder append(String str) {
+        if (str == null) {
+            return this;
+        }
+
+        saveToHistory();
+        int strLen = str.length();
+        ensureCapacity(size + strLen);
+        str.getChars(0, strLen, value, size);
+        size += strLen;
+
+        return this;
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > value.length) {
+            expandCapacity(minCapacity);
+        }
+    }
+
+    private void expandCapacity(int minCapacity) {
+        int newCapacity = value.length * 2;
+        if (newCapacity < minCapacity) {
+            newCapacity = minCapacity;
+        }
+        value = Arrays.copyOf(value, newCapacity);
+    }
+
+    public CustomStringBuilder insert(int index, String str) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (str == null) {
+           return this;
+        }
+        saveToHistory();
+        int strLen = str.length();
+        ensureCapacity(size + strLen);
+
+        System.arraycopy(value, index, value, index + strLen, size - index);
+
+        str.getChars(0, strLen, value, index);
+        size += strLen;
+
+        return this;
+    }
+
+    public CustomStringBuilder delete(int start, int end) {
+
+        if (start < 0 || end > size || start >= end) {
+            throw new IndexOutOfBoundsException("Invalid start or end index");
+        }
+
+        saveToHistory();
+        System.arraycopy(value, end, value, start, size - end);
+        size -= (end - start);
+
+        return this;
+    }
+
+    public void undo() {
+        if (!history.isEmpty()) {
+            CustomStringBuilderMemento memento = history.remove(history.size() - 1);
+            value = memento.getState();
+            size = memento.getSize();
+        }
+    }
+
+    private void saveToHistory() {
+        history.add(new CustomStringBuilderMemento(value, size));
+    }
+
+    @Override
+    public String toString() {
+        return new String(value, 0, size);
+    }
+}
+
+
+class CustomStringBuilderMemento {
+    private char[] state;
+    private int size;
+
+    public CustomStringBuilderMemento(char[] state, int size) {
+        this.state = Arrays.copyOf(state, state.length);
+        this.size = size;
+    }
+
+    public char[] getState() {
+        return Arrays.copyOf(state, state.length);
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
